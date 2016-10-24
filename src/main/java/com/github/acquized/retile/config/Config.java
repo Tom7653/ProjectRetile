7f44973496b24fcb460574db8d69b67963dac768
Acquized <flare.returns@outlook.com>
2016-10-24 20:17:50 +0200

    [Prepare] v1.0.0-PRERELEASEd

--- src/main/java/com/github/acquized/retile/config/Config.java
+++ src/main/java/com/github/acquized/retile/config/Config.java
@@ -67,7 +67,7 @@ public class Config extends YamlConfig {
 
     @Comment("Don't change this value unless you want to reset the Config")
     @Path("ProjectRetile.Version")
-    public String version = "1.0.0-PRERELEASEc";
+    public String version = "1.0.0-PRERELEASEd";
 
     // v  Aliases  v
 
